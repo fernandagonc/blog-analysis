58928cc371b44477123aef6601476ac9ce4c3880
fernandagonc <fernandarmg@gmail.com>
2021-12-09 22:07:40 -0300

    first commit

---
+++ ladyscomics/spiders/example.py
@@ -0,0 +1,37 @@
+import scrapy
+from ladyscomics.items import LadyscomicsItem
+
+class LadysComicsSpider(scrapy.Spider):
+    name = "LadysComics"
+    start_urls = [
+            'http://ladyscomics.com.br/category/especiais',    ]
+ 
+    def parse(self, response):
+        print(response)
+        for href in response.xpath('//div[has-class("nav-previous")]//a/@href').getall():
+            print(href)
+            url = response.urljoin(href)
+            yield scrapy.Request(url, callback=self.parse, dont_filter=True)
+            
+        for href in response.xpath('//a[has-class("button-primary")]/@href').getall():
+            print(href)
+            url = response.urljoin(href)
+            yield scrapy.Request(url, callback=self.parse_dir_contents, dont_filter=True)
+
+    def parse_dir_contents(self, response):
+        item = LadyscomicsItem()
+        item['title'] = response.xpath('//h1[has-class("entry-title")]/text()').extract()
+        item['author'] = response.xpath('//a[has-class("author")]/text()').extract()
+        item['date'] = response.xpath('//a[has-class("entry-date")]/text()').extract_first()
+        item['body'] = [ ' '.join(
+            line.strip() 
+            for line in p.xpath('.//text()').extract() 
+            if line.strip())    
+            for p in response.xpath('//p')
+            ]
+            
+        if(response.xpath('//img').getall() != ''):
+            item['hasImage'] = True
+        else:
+             item['hasImage'] = False
+        yield item
