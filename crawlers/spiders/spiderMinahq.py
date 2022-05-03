9113e0090c4e165aeb8453c709cc235bd987ce21
fernandagonc <fernandarmg@gmail.com>
2022-05-02 21:19:19 -0300

    correção categoria

--- crawlers/spiders/spiderMinahq.py
+++ crawlers/spiders/spiderMinahq.py
@@ -3,14 +3,20 @@ from crawlers.items import TextItem
 
 class LadysComicsSpider(scrapy.Spider):
     name = "MinaHQ"
-    start_urls = ['https://minadehq.com.br/category/quadrinistas-mulheres-e-nao-binarias/',]
+    start_urls = [
+    'https://minadehq.com.br/category/quadrinistas-mulheres-e-nao-binarias/',
+    'https://minadehq.com.br/category/newsletter/',
+    'https://minadehq.com.br/category/quadrinhos-exclusivos/',
+    'https://minadehq.com.br/category/dicas-e-resenhas-de-quadrinhos/',
+    'https://minadehq.com.br/category/noticias/',
+    ]
 
     def parse(self, response):
         print(response)
-        # for href in response.xpath('//div[has-class("paginator")]//a[has-class("nav-next")]/@href').getall():
-        #     print(href)
-        #     url = response.urljoin(href)
-        #     yield scrapy.Request(url, callback=self.parse, dont_filter=True)
+        for href in response.xpath('//div[has-class("paginator")]//a[has-class("nav-next")]/@href').getall():
+            print(href)
+            url = response.urljoin(href)
+            yield scrapy.Request(url, callback=self.parse, dont_filter=True)
 
         for href in response.xpath('//h3[has-class("entry-title")]//a/@href').getall():
             print(href)
@@ -34,5 +40,5 @@ class LadysComicsSpider(scrapy.Spider):
         else:
              item['hasImage'] = False
 
-        item['category'] = response.xpath('//a[has-class("category-link")]/a/text()').extract()
+        item['category'] = response.xpath('//span[has-class("category-link")]/a/text()').extract()
         yield item
